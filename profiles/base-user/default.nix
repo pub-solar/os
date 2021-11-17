cfe5d77f9e83322b96318dc48a2048b1384d1c07
Benjamin Bädorf <hello@benjaminbaedorf.eu>
2021-11-17 12:05:50 +0100

    Allow changing the user description

--- profiles/base-user/default.nix
+++ profiles/base-user/default.nix
@@ -12,7 +12,7 @@ in
     # This automatically sets group to users, createHome to true,
     # home to /home/username, useDefaultShell to true, and isSystemUser to false.
     isNormalUser = true;
-    description = "The main PubSolarOS user";
+    description = psCfg.user.description;
     extraGroups = [ "wheel" "docker" "input" "audio" "networkmanager" "lp" "scanner" ];
     initialHashedPassword = if psCfg.user.password != null then psCfg.user.password else "";
     shell = pkgs.zsh;
