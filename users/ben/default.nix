43d421bcac6f5fda7eb99273b4d72d984e660220
Benjamin Bädorf <hello@benjaminbaedorf.eu>
2021-11-17 19:12:41 +0100

    Update user description

--- users/ben/default.nix
+++ users/ben/default.nix
@@ -16,7 +16,7 @@ in
       # The rest is used for programs like git
       user = {
         name = "ben";
-        description = "Benjamin Bädorf";
+        description = "b12f";
         password = "$6$LO2YoaHwuRQhUoSz$iHw9avM887eJg9cIty2nmG4Ibkol3YpviEhYpivVQP31VrnihFz/6LyugxD7X4VmXx9nxvcYIZnN90rlGxwjT.";
         fullName = "Benjamin Bädorf";
         email = "hello@benjaminbaedorf.eu";
