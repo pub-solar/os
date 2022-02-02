0a1cd6bf40ad7dcab949edfb425cebc0d3cb59d7
teutat3s <teutates@mailbox.org>
2022-02-02 21:23:35 +0100

    Remove SSH public key, add comment about default password

--- users/pub-solar/default.nix
+++ users/pub-solar/default.nix
@@ -9,10 +9,11 @@
     # The rest is used for programs like git
     user = {
       name = "pub-solar";
+      # default password = pub-solar
       password = "$6$Kv0BCLU2Jg7GN8Oa$hc2vERKCbZdczFqyHPfgCaleGP.JuOWyd.bfcIsLDNmExGXI6Rnkze.SWzVzVS311KBznN/P4uUYAUADXkVtr.";
       fullName = "Pub Solar";
       email = "iso@pub.solar";
-      publicKeys = [ ./public-keys/teutat3s-yubikey.pub ];
+      publicKeys = [ ];
     };
   };
 }
