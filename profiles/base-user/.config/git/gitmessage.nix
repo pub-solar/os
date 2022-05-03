f4a2cc34bb3b913717fd6402b986219fde453ef3
Benjamin Bädorf <hello@benjaminbaedorf.eu>
2022-05-03 12:43:32 +0200

    Comment Co-Authored-by

--- profiles/base-user/.config/git/gitmessage.nix
+++ profiles/base-user/.config/git/gitmessage.nix
@@ -4,20 +4,19 @@ let
   xdg = config.home-manager.users."${user.name}".xdg;
 in
 ''
-  # Title: Summary, imperative, start upper case, don't end with a period
-  # No more than 50 chars. #### 50 chars is here:  #
-  #
+# Title: Summary, imperative, start upper case, don't end with a period
+# No more than 50 chars. #### 50 chars is here:  #
+#
 
 
-  # ^ Remember ending with an extra blank line
-  # Body: Explain *what* and *why* (not *how*). Include issue number.
-  # Wrap at 72 chars. ################################## which is here:  #
-  #
+# ^ Remember ending with an extra blank line
+# Body: Explain *what* and *why* (not *how*). Include issue number.
+# Wrap at 72 chars. ################################## which is here:  #
+#
 
 
-  # ^ Remember ending with an extra blank line
-  # At the end: Include Co-authored-by for all contributors.
-  #
-  Co-authored-by: ${user.fullName} <${user.email}>
-
+# ^ Remember ending with an extra blank line
+# At the end: Include Co-authored-by for all contributors.
+#
+# Co-authored-by: Example Name <email@example.com>
 ''
